e691e2a1c0a1db0c80300c07e8bb2909db5048cb
BrunoVieira005 <brunorv2005@gmail.com>
2025-03-27 19:17:46 -0300

    Finalizado o exercício
    Resolução do erro no Widget build, mudança no campo de texto que exibe a variável nome e mudança dos ícones que funcionam como botão

--- appconttech/lib/main.dart
+++ appconttech/lib/main.dart
@@ -29,9 +29,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // Variavel Contador
+  // Variáveis Contador e Nome
   int contador = 0;
   String nome = "";
+
   // Cria função para incrementar
   _incrementar() {
     setState(() {
@@ -56,61 +57,60 @@ class _HomeState extends State<Home> {
     setState(() {
       nome = "";
     });
+  }
 
-    @override
-    Widget(BuildContext context) {
-      return Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                width: 200,
-                height: 100,
-                alignment: Alignment.center,
-                color: Colors.blueAccent,
-                child: Text(
-                  "Contador: ${contador}",
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 100,
+              alignment: Alignment.center,
+              color: Colors.blueAccent,
+              child: Text(
+                "Contador: ${contador}",
+                style: TextStyle(fontSize: 30, color: Colors.white),
+                textAlign: TextAlign.center,
               ),
-            ],
-          ),
-          SizedBox(
-            height: 250,
-          ),
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                width: 200,
-                height: 100,
-                alignment: Alignment.center,
-                color: Colors.blueAccent,
-                child: Text(
-                  "${nome}",
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 250,
+        ),
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 100,
+              alignment: Alignment.center,
+              color: Colors.blueAccent,
+              child: Text(
+                nome,
+                style: TextStyle(fontSize: 30, color: Colors.white),
+                textAlign: TextAlign.center,
               ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // ElevatedButton(onPressed: _exibemsg, child: Text("Mensagem"),),
-              IconButton(onPressed: _incrementar, icon: Icon(Icons.plus_one)),
-              IconButton(onPressed: _resetar, icon: Icon(Icons.restart_alt)),
-              IconButton(onPressed: _exibeNome, icon: Icon(Icons.show_chart)),
-              IconButton(onPressed: _limparNome, icon: Icon(Icons.remove)),
-            ],
-          ),
-        ],
-      );
-    }
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(onPressed: _incrementar, icon: Icon(Icons.plus_one)),
+            IconButton(onPressed: _resetar, icon: Icon(Icons.restart_alt)),
+            IconButton(onPressed: _exibeNome, icon: Icon(Icons.visibility)),
+            IconButton(
+                onPressed: _limparNome, icon: Icon(Icons.visibility_off)),
+          ],
+        ),
+      ],
+    );
   }
 }
